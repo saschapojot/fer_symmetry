3fd0228eef61bdc82877636a8ad73726710dd0d2
Xi LIU <xi.liu@u.nus.edu>
2025-05-08 21:34:17 +0800

    plt

--- ising/plt/fit_chi.py
+++ ising/plt/fit_chi.py
@@ -19,13 +19,13 @@ if (len(sys.argv)!=3):
 
 init_path=int(sys.argv[1])
 row=sys.argv[2]
-N_vec=np.array([32,64,128])
+N_vec=np.array([32,64])
 
 csvDataFolderRoot_vec=[f"../dataAll/N{N}/row{row}/csvOut_init_path{init_path}/" for N in N_vec]
 csv_out_dir=f"../dataAll/row{row}/"
 Path(csv_out_dir).mkdir(exist_ok=True,parents=True)
 
-Tc=11.31722889
+Tc=1.10448337
 def chi_func(T,alpha,beta,gamma):
     return alpha*(T-Tc)**(-gamma)+beta
 
@@ -36,7 +36,7 @@ inCsvFile0=csvDataFolderRoot_vec[N0_ind]+"/magnetization_plot.csv"
 df0=pd.read_csv(inCsvFile0)
 TVec0=np.array(df0["T"])
 chi0=np.array(df0["chi_each_site"])
-mask0=(TVec0>11.38) &( TVec0<12)
+mask0=(TVec0>1.1) &( TVec0<1.6)
 TInds0 = np.where(mask0)[0]
 T_vec0_fit=TVec0[TInds0]
 chi0_fit=chi0[TInds0]
@@ -57,7 +57,7 @@ inCsvFile1=csvDataFolderRoot_vec[N1_ind]+"/magnetization_plot.csv"
 df1=pd.read_csv(inCsvFile1)
 TVec1=np.array(df1["T"])
 chi1=np.array(df1["chi_each_site"])
-mask1=(TVec1>11.33)
+mask1=(TVec0>1.1) &( TVec0<1.6)
 TInds1 = np.where(mask1)[0]
 T_vec1_fit=TVec1[TInds1]
 chi1_fit=chi1[TInds1]
@@ -70,51 +70,51 @@ init_guess1=[1,1,-1]
 # print(f"Fitted parameters for {N_vec[N1_ind]}:\n alpha1 = {alpha_fit1}\n beta1  = {beta_fit1}\n gamma1 = {gamma_fit1}")
 
 #N2_ind
-N2_ind=2#128
-inCsvFile2=csvDataFolderRoot_vec[N2_ind]+"/magnetization_plot.csv"
-df2=pd.read_csv(inCsvFile2)
-TVec2=np.array(df2["T"])
-chi2=np.array(df2["chi_each_site"])
-mask2=(TVec2>11.33)
-TInds2 = np.where(mask2)[0]
-T_vec2_fit=TVec2[TInds2]
-chi2_fit=chi2[TInds2]
-print(f"T_vec2_fit={T_vec2_fit}")
-print(f"chi2_fit={chi2_fit}")
+# N2_ind=2#128
+# inCsvFile2=csvDataFolderRoot_vec[N2_ind]+"/magnetization_plot.csv"
+# df2=pd.read_csv(inCsvFile2)
+# TVec2=np.array(df2["T"])
+# chi2=np.array(df2["chi_each_site"])
+# mask2=(TVec2>11.33)
+# TInds2 = np.where(mask2)[0]
+# T_vec2_fit=TVec2[TInds2]
+# chi2_fit=chi2[TInds2]
+# print(f"T_vec2_fit={T_vec2_fit}")
+# print(f"chi2_fit={chi2_fit}")
 
 
-init_guess2=[1,1,-1]
+# init_guess2=[1,1,-1]
 # popt2, pcov2 = curve_fit(chi_func, T_vec2_fit, chi2_fit, p0=init_guess2,maxfev=10000)
 # # Extract the best-fit parameters.
 # alpha_fit2, beta_fit2, gamma_fit2 = popt2
 # print(f"Fitted parameters for {N_vec[N2_ind]}:\n alpha2 = {alpha_fit2}\n beta2  = {beta_fit2}\n gamma2 = {gamma_fit2}")
 
 #for N1
-beta_estimate1=0
-T1_fit_linear=np.log(T_vec1_fit-Tc)
-model1=LinearRegression()
-model1.fit(T1_fit_linear.reshape(-1,1),np.log(chi1_fit-beta_estimate1))
+# beta_estimate1=0
+# T1_fit_linear=np.log(T_vec1_fit-Tc)
+# model1=LinearRegression()
+# model1.fit(T1_fit_linear.reshape(-1,1),np.log(chi1_fit-beta_estimate1))
 # Extract the slope and intercept
-slope1 = model1.coef_[0]
-intercept1 = model1.intercept_
-print(f"Slope1: {slope1:.3f}, Intercept1: {intercept1:.3f}")
+# slope1 = model1.coef_[0]
+# intercept1 = model1.intercept_
+# print(f"Slope1: {slope1:.3f}, Intercept1: {intercept1:.3f}")
 
 #for N2
-beta_estimate2=0
-T2_fit_linear=np.log(T_vec2_fit-Tc)
-model2=LinearRegression()
-model2.fit(T2_fit_linear.reshape(-1,1),np.log(chi2_fit-beta_estimate2))
+# beta_estimate2=0
+# T2_fit_linear=np.log(T_vec2_fit-Tc)
+# model2=LinearRegression()
+# model2.fit(T2_fit_linear.reshape(-1,1),np.log(chi2_fit-beta_estimate2))
 # Extract the slope and intercept
-slope2 = model2.coef_[0]
-intercept2 = model2.intercept_
-print(f"Slope2: {slope2:.3f}, Intercept2: {intercept2:.3f}")
+# slope2 = model2.coef_[0]
+# intercept2 = model2.intercept_
+# print(f"Slope2: {slope2:.3f}, Intercept2: {intercept2:.3f}")
 
 
 
-chi0_max=38.0525885224236
-chi1_max=134.352103730896
-chi2_max=268.708344191767
-chi_max_vec=[chi0_max,chi1_max,chi2_max]
+chi0_max=107.490063078703
+chi1_max=116.303351945529
+# chi2_max=268.708344191767
+chi_max_vec=[chi0_max,chi1_max]
 model_L=LinearRegression()
 model_L.fit(np.log(np.array(N_vec)).reshape(-1,1),np.log(chi_max_vec))
 slopeL = model_L.coef_[0]
---
+++ ising/plt/load_csv_plt_s00_corr.py
@@ -0,0 +1,135 @@
+import numpy as np
+import glob
+import sys
+import re
+import matplotlib.pyplot as plt
+from datetime import datetime
+import json
+import pandas as pd
+from pathlib import Path
+import scipy.stats as stats
+from sklearn.linear_model import LinearRegression
+#this script loads correlation function csv files
+# for s00 and s_{ij}
+# for all T
+
+
+if (len(sys.argv)!=4):
+    print("wrong number of arguments")
+    exit()
+
+N=int(sys.argv[1])
+init_path=int(sys.argv[2])
+row=sys.argv[3]
+csvDataFolderRoot=f"../dataAll/N{N}/row{row}/csvOut_init_path{init_path}/"
+inCsvFile=csvDataFolderRoot+"/magnetization_plot.csv"
+s00_corr_dir=csvDataFolderRoot+"/s00_corr/"
+s00_fit_dir=csvDataFolderRoot+"/s00_fit/"
+Path(s00_corr_dir).mkdir(exist_ok=True,parents=True)
+Path(s00_fit_dir).mkdir(exist_ok=True,parents=True)
+
+TVals=[]
+TFileNames=[]
+for TFile in glob.glob(csvDataFolderRoot+"/T*"):
+
+    matchT=re.search(r"T([-+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?)",TFile)
+    # if float(matchT.group(1))<1:
+    #     continue
+
+    if matchT:
+        TFileNames.append(TFile)
+        TVals.append(float(matchT.group(1)))
+
+sortedInds=np.argsort(TVals)
+sortedTVals=[TVals[ind] for ind in sortedInds]
+sortedTFiles=[TFileNames[ind] for ind in sortedInds]
+
+
+def plt_s00_corr_one_T(oneTFile):
+    matchT=re.search(r'T([-+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?)',oneTFile)
+    TStr=matchT.group(1)
+    s00_corr_csv_file_name=oneTFile+"/s00_corr.csv"
+
+    s00_corr_arr=np.array(pd.read_csv(s00_corr_csv_file_name,header=None))
+
+    dx=np.minimum(s00_corr_arr[:,0],N-s00_corr_arr[:,0])
+    dy=np.minimum(s00_corr_arr[:,1],N-s00_corr_arr[:,1])
+
+    distances=np.sqrt(dx**2+dy**2)
+    unique_distances = np.unique(distances)
+    # print(len(unique_distances))
+    # Compute the average s00_corr for each unique distance
+    avg_corr = np.array([
+        np.mean(s00_corr_arr[distances == d, 2]) for d in unique_distances
+    ])
+
+    mask=(distances>10) & (distances<20)
+    filtered_distances = distances[mask]
+    filtered_corr = s00_corr_arr[mask, 2]
+    # Ensure no non-positive values (logarithm is not defined for zero or negative values)
+    valid = (filtered_distances > 0) & (filtered_corr > 0)
+    filtered_distances = filtered_distances[valid]
+    filtered_corr = filtered_corr[valid]
+    # Take the logarithm of the filtered data
+    log_dist = np.log(filtered_distances)
+    log_corr = np.log(filtered_corr)
+    X = log_dist.reshape(-1, 1)
+    y = log_corr
+    # Create and fit the linear regression model
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Extract slope (coefficient) and intercept
+    slope = model.coef_[0]
+    intercept = model.intercept_
+    # Calculate R-squared score of the model
+    r_squared = model.score(X, y)
+
+    # Print regression statistics
+    print("======================")
+    print(f"T={TStr}")
+    print(f"Slope: {slope:.3f}")
+    print(f"Intercept: {intercept:.3f}")
+    print(f"R-squared: {r_squared:.3f}")
+
+
+
+    plt.scatter(unique_distances, avg_corr, color='red', alpha=0.8)
+    plt.xlabel('Distance from [0,0]')
+    plt.ylabel('$<s_{00}s_{ij}>$')
+    plt.title(r'Scatter Plot of Distance vs $<s_{00}s_{ij}>$ at T = '+str(TStr))
+    plt.grid(True)
+    plt.savefig(s00_corr_dir+f"/s00_corr_T{TStr}.png")
+    plt.close()
+
+    #plot log log fit
+    if r_squared>0.1:
+        plt.figure(figsize=(8, 6))
+        plt.scatter(X, y, color='blue', alpha=0.7)
+        # Generate points for the regression line
+        x_line = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
+        y_line = model.predict(x_line)
+        plt.plot(x_line, y_line, label=f'Fit: slope = {slope:.3f}', color='red')
+        plt.xlabel('log(Distance)')
+        plt.ylabel('log(s00_corr)')
+        plt.title(f'Log-Log Linear Regression at T = {TStr}')
+        # plt.legend()
+        plt.grid(True)
+        plt.savefig(s00_fit_dir+f"/s00_fit_T{TStr}.png")
+        plt.close()
+
+
+
+
+tStart=datetime.now()
+# print(sortedTFiles[0])
+# plt_s00_corr_one_T(sortedTFiles[0])
+
+for k in range(0,len(sortedTFiles)):
+    oneTFile=sortedTFiles[k]
+    plt_s00_corr_one_T(oneTFile)
+    print(f"{oneTFile}")
+
+tEnd=datetime.now()
+
+print(f"time: {tEnd-tStart}")
