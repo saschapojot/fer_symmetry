f974ac375fa6180ffb07d5fe11091cc95d69544f
Xi LIU <xi.liu@u.nus.edu>
2025-05-14 20:54:50 +0800

    sh

---
+++ ising/gen_exec_noChecking_s_gansu.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from decimal import Decimal, getcontext
+import shutil
+import numpy as np
+import pandas as pd
+import os
+#this script creates slurm bash files for exec_noChecking_s.py
+
+def format_using_decimal(value, precision=4):
+    # Set the precision higher to ensure correct conversion
+    getcontext().prec = precision + 2
+    # Convert the float to a Decimal with exact precision
+    decimal_value = Decimal(str(value))
+    # Normalize to remove trailing zeros
+    formatted_value = decimal_value.quantize(Decimal(1)) if decimal_value == decimal_value.to_integral() else decimal_value.normalize()
+    return str(formatted_value)
+
+outPath="./bashFiles_dipole_exec_noChecking/"
+if os.path.isdir(outPath):
+    shutil.rmtree(outPath)
+
+
+Path(outPath).mkdir(exist_ok=True,parents=True)
+N=150#unit cell number
+startingFileIndSuggest=8
+init_path=0
+which_row=0
+chunk_size = 100
+T_vec1=[0.5+0.1*n for n in range(0,6)]+[0.5+0.1*n for n in range(8,16)]
+T_vec2=[1.12+0.02*n for n in range(0,5)]#1.12, 1.14, 1.16, 1.18, 1.2
+T_vec3=[1.13,1.15,1.17,1.19]
+T_vec4=[1.21,1.22,1.23,1.24,1.25]
+TVals=T_vec1+T_vec2+T_vec3+T_vec4
+
+
+chunks = [TVals[i:i + chunk_size] for i in range(0, len(TVals), chunk_size)]
+
+def contents_to_bash(chk_ind,T_ind,chunks):
+    TStr=format_using_decimal(chunks[chk_ind][T_ind])
+    contents=[
+        "#!/bin/bash\n",
+        "#SBATCH -n 4\n",
+        "#SBATCH -N 1\n",
+        "#SBATCH -t 0-60:00\n",
+        "#SBATCH -p lzicnormal\n",
+        "#SBATCH --mem=8GB\n",
+        f"#SBATCH -o out_exec_noChecking_s_{TStr}.out\n",
+        f"#SBATCH -e out_exec_noChecking_s_{TStr}.err\n",
+        "cd /public/home/hkust_jwliu_1/liuxi/Document/cppCode/fer_symmetry/ising\n",
+        f"python3 -u exec_noChecking_s.py {TStr} {N} {startingFileIndSuggest} {init_path} {which_row}\n"
+        ]
+    out_chunk=outPath+f"/chunk{chk_ind}/"
+    Path(out_chunk).mkdir(exist_ok=True,parents=True)
+    outBashName=out_chunk+f"/exec_checking_pol_T{TStr}.sh"
+    with open(outBashName,"w+") as fptr:
+        fptr.writelines(contents)
+
+
+for chk_ind in range(0,len(chunks)):
+    for T_ind in range(0,len(chunks[chk_ind])):
+        contents_to_bash(chk_ind,T_ind,chunks)
